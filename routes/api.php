92e9ee0fa393e16a7e79d7233d4197534408cfee
Andrew Brooks <dalebrookscontact@gmail.com>
2023-07-09 15:42:09 -0700

    Base work to support token based authorization - uses Laravel/Sanctum for auth - register and login routes will now generate user tokens - /login will validate credentials - trait added for ease of handling success/error HTTP responses - validation rules and auth checks added to HTTP requests for /login /register - placeholder added for /logout

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Auth routes
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
